df1d781324d47ff68559b15982d6ad21eb0631ff
Ahmed78696 <k00278351@student.tus.ie>
2025-04-08 09:26:05 +0100

    add

--- src/bank/CurrentAccount.java
+++ src/bank/CurrentAccount.java
@@ -8,20 +8,19 @@ public class CurrentAccount extends Account {
         this.overdraftLimit = overdraftLimit;
     }
 
-    @Override
-    public void withdraw(double amount) {
-        if (amount > getBalance() + overdraftLimit) {
-            throw new IllegalArgumentException("Insufficient funds or overdraft limit exceeded.");
-        }
-        setBalance(getBalance() - amount);
-    }
-
     public double getOverdraftLimit() {
         return overdraftLimit;
     }
 
     public void setOverdraftLimit(double overdraftLimit) {
         this.overdraftLimit = overdraftLimit;
+    }
+    @Override
+    public void withdraw(double amount) {
+        if (amount > getBalance() + overdraftLimit) {
+            throw new IllegalArgumentException("Insufficient funds or overdraft limit exceeded.");
+        }
+        setBalance(getBalance() - amount);
     }
 }
 
